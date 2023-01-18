fd08638e165ccb7c2a9cac02dc2a95886f736b96
simonatingleffkardel <simonakardel@gmail.com>
2023-01-18 13:30:26 +0100

    student

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -4,6 +4,7 @@ import model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import repo.Repository;
 
@@ -11,13 +12,14 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/")
 public class Controller {
 
     @Autowired
     Repository repository;
 
 
-    @GetMapping("/students")
+    @GetMapping
     public List<Student> students() {
         return repository.findAll();
     }
